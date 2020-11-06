a39ccf9e40eea44b4dfe5e80e4f851b017e74ad3
rachel rafik <rrafik4358@stu.d214.org>
2020-11-06 14:21:31 -0600

    Initial Commit

---
+++ viewLecture1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viewLecture1
+//
+//  Created by Rachel Rafik on 11/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
